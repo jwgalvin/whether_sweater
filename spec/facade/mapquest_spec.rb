9cd0981031c1b1494de0b44e1d06d3c8c5b588ed
Joseph W Galvin <jwgalvin@gmail.com>
2022-04-24 15:24:54 -0600

    test: mapquest_spec added.

---
+++ spec/facade/mapquest_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe MapquestFacade do
+  it 'gets the gps data points', :vcr do
+    gps = MapquestFacade.geocode("denver, co")
+    expect(gps).to be_a(Coord)
+  end
+
+  it "tests the routing I imported from other IC", :vcr do
+    start = 'denver, co'
+    destination = 'ft.collins, co'
+    trip = MapquestFacade.route(start, destination)
+    #binding.pry
+    expect(trip).to be_a(Route)
+    expect(trip.distance).to be_a(Float)
+    expect(trip.eta).to be_a(String)
+    expect(trip.parse_directions).to be_a(Array)
+  end
+end
